fa36395c6b9bed35db7f7c2f9256e8fe5679141f
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2020-09-07 14:29:58 +0200

    init commit

---
+++ desktop/src/com/abalone/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.abalone.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.abalone.game.AbaloneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AbaloneGame(), config);
+	}
+}
