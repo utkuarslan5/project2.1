69f8b3c8c98ac20df1a4aedc40951f4228558b8e
ymavruk <56226042+ymavruk@users.noreply.github.com>
2020-09-12 21:34:00 +0200

    play background and board

--- core/src/com/abalone/game/objects/Board.java
+++ core/src/com/abalone/game/objects/Board.java
@@ -1,4 +1,16 @@
 package com.abalone.game.objects;
 
+import com.abalone.game.AbaloneGame;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
 public class Board {
+    private Image board;
+    public Board(){
+        Texture img = new Texture("abalone.png");
+        board = new Image(img);
+    }
+    public Image getBoardImage(){
+        return board;
+    }
 }
--- core/src/com/abalone/game/states/PlayState.java
+++ core/src/com/abalone/game/states/PlayState.java
@@ -1,8 +1,25 @@
 package com.abalone.game.states;
 
+import com.abalone.game.AbaloneGame;
 import com.abalone.game.managers.GameStateManager;
+import com.abalone.game.objects.Board;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PlayState extends State {
+    private SpriteBatch spriteBatch;
+    private Stage stage;
+    private Image background;
+    private BitmapFont gameFont;
+    private Board board;
 
     protected PlayState(GameStateManager ourGsm) {
         super(ourGsm);
@@ -10,7 +27,22 @@ public class PlayState extends State {
 
     @Override
     public void init() {
-
+        spriteBatch = new SpriteBatch();
+        board = new Board();
+        board.getBoardImage().setPosition(AbaloneGame.width/2f-(board.getBoardImage().getWidth()/2),AbaloneGame.height/2f- (board.getBoardImage().getHeight()/2));
+        Viewport viewport = new FitViewport(AbaloneGame.width, AbaloneGame.height, AbaloneGame.cam);
+        viewport.apply();
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("Prime-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 80;
+        gameFont = gen.generateFont(parameter);
+        gameFont.setColor(Color.LIGHT_GRAY);
+        stage = new Stage(viewport, spriteBatch);
+        Gdx.input.setInputProcessor(stage);
+        Texture img = new Texture("pbg.jpg");
+        background = new Image(img);
+        background.setWidth(AbaloneGame.width);
+        background.setHeight(AbaloneGame.height);
     }
 
     @Override
@@ -20,7 +52,10 @@ public class PlayState extends State {
 
     @Override
     public void draw() {
-
+        stage.addActor(background);
+        stage.addActor(board.getBoardImage());
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
