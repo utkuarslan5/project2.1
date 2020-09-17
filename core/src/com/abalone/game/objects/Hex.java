b9996f6e6ed2ab7e9cea2b68b74c15c59678c406
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2020-09-17 12:13:25 +0200

    Hex.java finished

--- core/src/com/abalone/game/objects/Hex.java
+++ core/src/com/abalone/game/objects/Hex.java
@@ -1,21 +1,73 @@
 package com.abalone.game.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hex {
     private int x;
     private int z;
+    private List<Hex> directions = new ArrayList<>();
 
-    public Hex(int x,int z){
+    public Hex(int x, int z) {
         this.x = x;
         this.z = z;
+        generateDirections();
+    }
+
+    public void generateDirections() {
+        directions.add(new Hex(0, 1));
+        directions.add(new Hex(0, -1));
+        directions.add(new Hex(1, 0));
+        directions.add(new Hex(-1, 0));
+        directions.add(new Hex(-1, 1));
+        directions.add(new Hex(1, -1));
+    }
+
+    public void add(Hex b) {
+        this.x += b.x;
+        this.z += b.z;
+    }
+
+    public void subtract(Hex b) {
+        this.x -= b.x;
+        this.z -= b.z;
+    }
+
+    public List<Hex> getDirections() {
+        return directions;
+    }
+
+    private List<Hex> getNeighbors() {
+        List<Hex> neighbors = new ArrayList<>();
+        for (Hex neighbor : directions) {
+            neighbors.add(new Hex(this.x + neighbor.x, this.z + neighbor.z));
+        }
+        return neighbors;
+    }
+
+    private boolean isNeighbor(Hex b) {
+        List<Hex> neighbors = getNeighbors();
+        for (Hex neighbor : neighbors) {
+            if (neighbor.x == b.x && neighbor.z == b.z) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private float calculateDistance(Hex b) {
+        return (Math.abs(this.x - b.x) + Math.abs(getY() - b.getY()) + Math.abs(this.z - b.z)) / 2;
     }
 
     public int getY() {
-        return -x-z;
+        return -x - z;
     }
-    public int getX(){
+
+    public int getX() {
         return x;
     }
-    public int getZ(){
+
+    public int getZ() {
         return z;
     }
 }
