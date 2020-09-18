5ab757a67a4a6ff6d428974bfb0b6bd14c907623
AyseTheArk <ayse.arslan@student.maastrichtuniversity.nl>
2020-09-18 14:39:06 +0200

    Shady white balls

--- core/src/com/abalone/game/utils/Color.java
+++ core/src/com/abalone/game/utils/Color.java
@@ -2,26 +2,43 @@ package com.abalone.game.utils;
 
 public enum Color {
 
-    WHITE{
-        public boolean isWhite() {
-            return true;
+    BLANK{
+        public  boolean isBlank() { return true;}
+
+        public boolean isPurple() {
+            return false;
         }
 
-        public boolean isBlack() {
+        public boolean isBlue() {
             return false;
         }
     },
-    BLACK{
-        public boolean isWhite() {
+    PURPLE{
+        public  boolean isBlank() { return false;}
+
+        public boolean isPurple() {
+            return true;
+        }
+
+        public boolean isBlue() {
+            return false;
+        }
+    },
+    BLUE{
+        public  boolean isBlank() { return false;}
+
+        public boolean isPurple() {
             return false;
         }
 
-        public boolean isBlack() {
+        public boolean isBlue() {
             return true;
         }
     };
 
-    public abstract boolean isWhite();
+    public abstract boolean isBlank();
 
-    public abstract boolean isBlack();
+    public abstract boolean isPurple();
+
+    public abstract boolean isBlue();
 }
