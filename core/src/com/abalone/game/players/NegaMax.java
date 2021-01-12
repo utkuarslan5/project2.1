d63b0087298eb22025024a162a9f2d8eca2f8b77
AyseTheArk <ayse.arslan@student.maastrichtuniversity.nl>
2021-01-12 16:44:26 +0100

    Move Ordering lil fix & clean code

--- core/src/com/abalone/game/players/NegaMax.java
+++ core/src/com/abalone/game/players/NegaMax.java
@@ -8,39 +8,33 @@ import java.util.Collections;
 import java.util.List;
 
 public class NegaMax extends Player {
-    /*
-    PSEUDO CODE FOR NORMAL:
-    function negamax(node, depth, color):
-        if depth = 0 or node is a terminal node then
-            return color & the heuristic value of node
-        value := −∞
-        for each child of node do
-            value := max(value, −negamax(child, depth − 1, −color))
-        return value
-    */
+
     private Node bestNode;
     private int depth;
     private boolean maximizingPlayer;
     private Tree tree;
+    double infinity = Double.POSITIVE_INFINITY;
 
     public NegaMax(Node currentNode, int depth, boolean maximizingPlayer, Tree tree) {
         this.depth = depth;
         this.maximizingPlayer = maximizingPlayer;
         this.tree = tree;
-        bestNode = negamax(currentNode, this.depth, this.maximizingPlayer, -100000000f, 10000000f);
+        bestNode = negamax(currentNode, this.depth, this.maximizingPlayer, -infinity, infinity);
     }
 
     private Node negamax(Node currentNode, int depth, boolean maximizingPlayer, double alpha, double beta) {
+
         List<Node> theChildren = currentNode.getChildren();
         Collections.sort(theChildren,Collections.<Node>reverseOrder());
+
         if (depth == 0) {
             return tree.getRoot();
         }
+
         Node bestNode = null;
-        double value = -10000000;
+        double value = -infinity;
 
-
-        for (Node child : currentNode.getChildren()) {
+        for (Node child : theChildren) {
             if (child != null) {
                 double nodeValue = negamax(child, depth - 1, !maximizingPlayer, beta, alpha).getHeuristicsValue();
                 if (nodeValue > value) {
